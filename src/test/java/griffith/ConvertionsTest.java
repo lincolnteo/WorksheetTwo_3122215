2716adcec6fdcd7450a2cdebec863e01593644f7
MINGKERN TEO <mingkern.teo@student.griffith.ie>
2025-03-03 00:30:52 +0000

    Initial commit

---
+++ src/test/java/griffith/ConvertionsTest.java
@@ -0,0 +1,26 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConvertionsTest {
+
+	@Test
+	void euroToDollar() {
+	}
+
+	@Test
+	void dollarToEuro() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void stringToInteger() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void integerToString() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void switchCase() {
+	}
+}
