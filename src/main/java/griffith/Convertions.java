5dbd665673b31e74d6d0d52ac4bc013cfc526852
MINGKERN TEO <mingkern.teo@student.griffith.ie>
2025-03-03 00:35:12 +0000

    Initial Commit - Added Java stub class and Junit classes

---
+++ src/main/java/griffith/Convertions.java
@@ -0,0 +1,20 @@
+package griffith;
+
+public class Convertions {
+	public double euroToDollar(double euro){
+		return 0;
+	}
+	public double dollarToEuro(double dollar){
+		return 0;
+	}
+	public int stringToInteger(String val){
+
+		return 0;
+	}
+	public String integerToString(int val){
+		return null;
+	}
+	public String switchCase(String str){
+		return null;
+	}
+}
---
+++ src/test/java/griffith/ConvertionsTest.java
@@ -0,0 +1,27 @@
+package griffith;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ConvertionsTest {
+
+	@Test
+	void euroToDollar() {
+	}
+
+	@Test
+	void dollarToEuro() {
+	}
+
+	@Test
+	void stringToInteger() {
+	}
+
+	@Test
+	void integerToString() {
+	}
+
+	@Test
+	void switchCase() {
+	}
+}
