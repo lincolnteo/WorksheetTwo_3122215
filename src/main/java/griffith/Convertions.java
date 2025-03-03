616bc8bab45cd8c0f135fb0d9e60630738c1288f
MINGKERN TEO <mingkern.teo@student.griffith.ie>
2025-03-03 17:58:23 +0000

    Seventh commit - written code to explain the implementation and funtion of the method

--- src/main/java/griffith/Convertions.java
+++ src/main/java/griffith/Convertions.java
@@ -2,42 +2,42 @@ package griffith;
 
 public class Convertions {
 	public double euroToDollar(double euro){
-		if (euro == 0 ) {
+		if (euro == 0 ) {// if euro == 0 it will be error as you cant convert nothing
 			return 404;
 		}
-		double dollar = euro * 1.2;
+		double dollar = euro * 1.2;// assuming the exchange rate is 1.2 dollar per euro
 		return dollar;
 	}
 	public double dollarToEuro(double dollar){
-		if (dollar == 0 ) {
+		if (dollar == 0 ) {// if dollar == 0 it will be error as you cant convert nothing
 			return 404;
 		}
-		return dollar * 0.8;
+		return dollar * 0.8;// assuming this exchange rate is 0.8 euro per dollar
 	}
 	public int stringToInteger(String val){
-		if(val.isBlank()){
+		if(val.isBlank()){// if the val is null it will return 404 as error
 			return 404;
 		}
-		int integer = Integer.parseInt(val);
-		return integer;
+		int integer = Integer.parseInt(val);// converting string to integer
+		return integer;// return converted
 	}
 	public String integerToString(int val){
-		if(val == 0){
+		if(val == 0){// if value == 0 it will return as an error
 			return "Error";
 		}
-		String string = Integer.toString(val);
-		return string;
+		String string = Integer.toString(val);//  int to string convertion
+		return string;// return converted
 	}
 	public String switchCase(String str){
-		if (str.isBlank()) {
+		if (str.isBlank()) {// if str == blank it will return as an error
 			return "Error";
 		}
-		if (str.matches("[A-Z]+")){
+		if (str.matches("[A-Z]+")){// if the str is in uppercase it will convert it to a lower case
 			return str.toLowerCase();
 		}
-		if(str.matches("[0-9]+")){
-			return "Error";
+		if(str.matches("[0-9]+")){// if the str has numbers it will be returned as an error because
+			return "Error";// numbers has no only one form
 		}
-		return str.toUpperCase();
+		return str.toUpperCase();// else it will return as upper case
 	}
 }
--- src/test/java/griffith/ConvertionsTest.java
+++ src/test/java/griffith/ConvertionsTest.java
@@ -9,7 +9,7 @@ class ConvertionsTest {
 	void euroToDollar() {
 		double eur = 100;
 		double eur1 = 200;
-		double eur2 = 0;
+		double eur2 = 0;// test error
 		assertEquals(120,con.euroToDollar(eur));
 		assertEquals(240,con.euroToDollar(eur1));
 		assertEquals(404,con.euroToDollar(eur2));
@@ -19,7 +19,7 @@ class ConvertionsTest {
 	void dollarToEuro() {
 		double dol = 100;
 		double dol1 = 200;
-		double dol2 = 0;
+		double dol2 = 0;// test error
 		assertEquals(80,con.dollarToEuro(dol));
 		assertEquals(160,con.dollarToEuro(dol1));
 		assertEquals(404,con.dollarToEuro(dol2));
@@ -30,7 +30,7 @@ class ConvertionsTest {
 	void stringToInteger() {
 		String str = "1234";
 		String str1 = "-12345";
-		String str2 = "";
+		String str2 = "";// test error
 		assertEquals(1234,con.stringToInteger(str));
 		assertEquals(-12345,con.stringToInteger(str1));
 		assertEquals(404,con.stringToInteger(str2));
@@ -40,7 +40,7 @@ class ConvertionsTest {
 	void integerToString() {
 		int str = 1234;
 		int str1 = -12345;
-		int str2 = 0;
+		int str2 = 0;// test error
 		assertEquals("1234",con.integerToString(str));
 		assertEquals("-12345",con.integerToString(str1));
 		assertEquals("Error",con.integerToString(str2));
@@ -52,7 +52,7 @@ class ConvertionsTest {
 		String str = "is";
 		String str1 = "IT";
 		String str2 = "working";
-		String str3 = "123465";
+		String str3 = "123465";// test error
 		assertEquals("IS",con.switchCase(str));
 		assertEquals("it",con.switchCase(str1));
 		assertEquals("WORKING",con.switchCase(str2));
